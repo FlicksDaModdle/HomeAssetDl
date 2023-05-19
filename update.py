f25771797fa5eae907c8ef8d7e47547ca0b70347
FlicksDaModdler <varunkrishnan2006@hotmail.com>
2023-05-19 11:17:29 -0400

    Updates All Files now

--- update.py
+++ update.py
@@ -1,20 +1,47 @@
 import os
 import requests
+import zipfile
+import shutil
 
-def download_and_replace_file(url):
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-    local_file_path = os.path.join(script_directory, os.path.basename(url))
+def update_folder_with_repository(username, repo_name):
+    # Create a temporary directory to store the downloaded repository
+    temp_dir = 'temp'
+    os.makedirs(temp_dir, exist_ok=True)
 
-    response = requests.get(url)
+    try:
+        # Download the repository as a zip file
+        download_url = f"https://github.com/FlicksDaModdle/HomeAssetDl/archive/refs/heads/main.zip"
+        response = requests.get(download_url)
 
-    if response.status_code == 200:
-        with open(local_file_path, 'wb') as file:
-            file.write(response.content)
-        print('File downloaded and replaced successfully.')
-    else:
-        print('Error downloading file.')
+        if response.status_code == 200:
+            # Save the zip file
+            zip_path = os.path.join(temp_dir, f"{repo_name}.zip")
+            with open(zip_path, 'wb') as zip_file:
+                zip_file.write(response.content)
 
-# Example usage:
-github_raw_file_url = 'https://raw.githubusercontent.com/FlicksDaModdle/HomeAssetDl/main/Pokemon_Asb_Downloader.py'
+            # Extract the zip file to the temporary directory
+            extract_dir = os.path.join(temp_dir, 'extracted')
+            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                zip_ref.extractall(extract_dir)
 
-download_and_replace_file(github_raw_file_url)
+            # Move the extracted files to the current directory
+            source_dir = os.path.join(extract_dir, f"{repo_name}-main")
+            for root, dirs, files in os.walk(source_dir):
+                for file in files:
+                    source_path = os.path.join(root, file)
+                    target_path = os.path.join(os.getcwd(), file)
+                    shutil.copy2(source_path, target_path)
+
+            print("Update completed successfully.")
+        else:
+            print("Failed to download the repository.")
+    except Exception as e:
+        print("An error occurred:", str(e))
+    finally:
+        # Remove the temporary directory
+        shutil.rmtree(temp_dir, ignore_errors=True)
+
+# Example usage
+repository_username = "FlicksDaModdle"
+repository_name = "HomeAssetDl"
+update_folder_with_repository(repository_username, repository_name)
